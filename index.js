c886ebccdc6f03499063aed84c3098e8dde98ecc
wildanzr <graita.sukma@gmail.com>
2022-10-28 23:30:24 +0700

    feat: base init

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('meea')
+require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Default route
+app.get('/', (req, res) => {
+  return res.send('Hello World')
+})
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
